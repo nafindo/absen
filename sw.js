cb5a9646238b30ab2627d85248f2752dcb130c88
Nafindo <nafindo@users.noreply.github.com>
2026-05-18 01:28:42 +0700

    Rearrange and rename menu grid based on company operational priorities: Row 1 operational tasks and news as Tugas Staf and Info & Promo, and Row 2 for administrative/salary, and bump PWA cache to v3.6

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('https://cdn.webpushr.com/sw-v2.js');
 
-const CACHE_NAME = 'absen-pwa-v3.5';
+const CACHE_NAME = 'absen-pwa-v3.6';
 const urlsToCache = [
   '/absen/',
   '/absen/index.html',
